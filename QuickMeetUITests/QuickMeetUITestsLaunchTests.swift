78201e8a2cd218030fabb0210d24997dc24b3b09
bozidarlabas <bozidar.labas@hotmail.com>
2024-02-05 19:31:05 +0100

    Project setup

---
+++ QuickMeetUITests/QuickMeetUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  QuickMeetUITestsLaunchTests.swift
+//  QuickMeetUITests
+//
+//  Created by Bozidar Labas on 05.02.2024..
+//
+
+import XCTest
+
+final class QuickMeetUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
